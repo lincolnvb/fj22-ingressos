0a2d343c7d9f3f1e3741144a01e52b2bd4f94fb8
Lincoln Viana Bassoto <lincolnvb@outlook.com>
2018-04-28 15:52:35 -0300

    Adicionado novas funcionalidades

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -27,7 +27,43 @@ public class Sessao {
 	
 	public Sessao(Filme filme, Sala sala, LocalTime horario){
 		this.filme = filme;
+		this.sala = sala;
+		this.horario = horario;
 		
 	}
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	
 	
 }
