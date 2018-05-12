b804c54a71807d2566bd5e02cce670e5287a8549
Lincoln Viana Bassoto <lincolnvb@outlook.com>
2018-05-12 17:17:09 -0300

    aula final.

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -4,17 +4,78 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Optional;
 
-import br.com.caelum.ingresso.model.descontos.Desconto;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Ingresso {
 
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@ManyToOne
 	private Sessao sessao;
+
+	@ManyToOne
+	private Lugar lugar;
+	
 	private BigDecimal preco;
 	
-	public Ingresso(Sessao sessao, Desconto desconto){
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso;
+	
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar){
 		this.sessao = sessao;
-		this.preco = desconto.aplicarDescontoSobre(sessao.getPreco());
+		this.tipoDeIngresso = tipoDeIngresso;
+		this.preco = this.tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+		
+		this.lugar = lugar;
 	}
+
+	public Ingresso (){
+		
+	};
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
+
+
+
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+
+
+
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
+	}
+
+
 
 	public Sessao getSessao() {
 		return sessao;
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,13 +3,19 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
@@ -28,6 +34,9 @@ public class Sessao {
 	
 	private BigDecimal preco;
 	
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+	
 	public Sessao(){};
 	
 	public Sessao(LocalTime horario,Filme filme, Sala sala){
@@ -42,6 +51,10 @@ public class Sessao {
 		return Optional.ofNullable(preco).orElse(BigDecimal.ZERO).setScale(2,  RoundingMode.HALF_UP);
 	}
 
+	public boolean isDisponivel(Lugar lugarSelecionado){
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+	
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
@@ -78,5 +91,7 @@ public class Sessao {
 		this.horario = horario;
 	}
 	
-	
+	public Map<String, List<Lugar>> getMapaDeLugares(){
+		return sala.getMapaDeLugares(); 
+	} 
 }
